ce47cfe85eb6d54321409bdfb61eb60da9002fe9
Karan <lakhadive1926@gmail.com>
2024-02-10 10:19:37 +0530

    bot modified

--- lib/screens/explore_screens/ai_screen.dart
+++ lib/screens/explore_screens/ai_screen.dart
@@ -1,91 +1,215 @@
-import 'dart:convert';
+// ignore_for_file: prefer_const_constructors
 
-import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:http/http.dart' as http;
+import 'package:lottie/lottie.dart';
+import 'package:myfit/bloc/chat_bloc.dart';
+import 'package:myfit/models/chat_message_models.dart';
 
 class AIScreen extends StatefulWidget {
-  const AIScreen({Key? key}) : super(key: key);
+  const AIScreen({super.key});
 
   @override
-  State<AIScreen> createState() => _AIScreenState();
+  State<AIScreen> createState() => AIScreenState();
 }
 
-class _AIScreenState extends State<AIScreen> {
-  ChatUser myself = ChatUser(id: '1', firstName: "Karan");
-  ChatUser bot = ChatUser(id: '2', firstName: "Gemini");
+class AIScreenState extends State<AIScreen> {
+  final ChatBloc chatBloc = ChatBloc();
+  TextEditingController textEditingController = TextEditingController();
 
-  List<ChatMessage> allMessages = [];
-  List<ChatUser> typing = []; 
+  String userName = "";
+    User? user = FirebaseAuth.instance.currentUser;
 
-  final url =
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyCqTKYLAt6qvt1ZTzYT17ySqX2Ei-jNbrk";
+    void fetchUser() async {
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user!.uid)
+          .snapshots()
+          .listen((DocumentSnapshot userDoc) {
+        setState(() {
+          userName = userDoc['name']; 
+        });
+      });
+    }
 
-  final header = {
-    'Content-Type': 'application/json',
-  };
+    String caps(String username) {
+      return username[0].toUpperCase()+username.substring(1);
+    }
 
-  getdata(ChatMessage m) async {
-    typing.add(bot);
-    allMessages.insert(0, m);
-    setState(() {});
-
-    var data = {
-      "contents": [
-        {
-          "parts": [
-            {"text": m.text}
-          ]
-        }
-      ]
-    };
-
-    await http
-        .post(Uri.parse(url), headers: header, body: jsonEncode(data))
-        .then((value) {
-      if (value.statusCode == 200) {
-        var res = jsonDecode(value.body);
-        print(res['candidates'][0]['content']['parts'][0]['text']);
-
-        ChatMessage m1 = ChatMessage(
-            user: bot,
-            createdAt: DateTime.now(),
-            text: res['candidates'][0]['content']['parts'][0]['text']);
-
-        allMessages.insert(0, m1);
-        
-      } else {
-        print("error");
-      }
-    });
-    typing.remove(bot);
-    setState(() {});
+  @override
+  void initState() {
+    // TODO: implement initState
+    fetchUser();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 243, 246, 249),
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Color.fromARGB(255, 243, 246, 249),
-          elevation: 0,
-          title: Text(
-            "Chat with AI",
-            style: GoogleFonts.poppins(
-                fontSize: 30,
-                fontWeight: FontWeight.w900,
-                color: Colors.blue[900]),
-          ),
-        ),
-        body: DashChat(
-          typingUsers: typing,
-          currentUser: myself,
-          onSend: (ChatMessage m) {
-            getdata(m);
-          },
-          messages: allMessages,
-        ));
+      body: BlocConsumer<ChatBloc, ChatState>(
+        bloc: chatBloc,
+        listener: (context, state) {},
+        builder: (context, state) {
+          switch (state.runtimeType) {
+            case ChatSuccessState:
+              List<ChatMessageModel> messages =
+                  (state as ChatSuccessState).messages;
+              return Container(
+                width: double.maxFinite,
+                height: double.maxFinite,
+                decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 243, 246, 249)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      height: 100,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Chat with AI",
+                            style: GoogleFonts.poppins(
+                                fontSize: 30,
+                                fontWeight: FontWeight.w900,
+                                color: Colors.blue[900]),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                        child: ListView.builder(
+                            itemCount: messages.length,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                  margin: const EdgeInsets.only(
+                                      bottom: 12, left: 16, right: 16),
+                                  padding: const EdgeInsets.all(16),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(16),
+                                      color: Color(
+                                          messages[index].role == "user"
+                                              ? 0xffd8e8ff
+                                              : 0xffE0F4FF)),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        messages[index].role == "user"
+                                            ? caps(userName)
+                                            : "Gemini",
+                                        style: GoogleFonts.poppins(
+                                            color:
+                                                messages[index].role == "user"
+                                                    ? Colors.blue[900]
+                                                    : Colors.blue[800],
+                                            fontWeight: FontWeight.w900,
+                                            fontSize: 20),
+                                      ),
+                                      const SizedBox(
+                                        height: 12,
+                                      ),
+                                      Text(messages[index].parts.first.text,
+                                          style: messages[index].role == "user"
+                                              ? GoogleFonts.poppins(
+                                                  height: 1.2,
+                                                  color:Color(0xff39A7FF),
+                                                  fontWeight: FontWeight.w800,
+                                                  fontSize: 18)
+                                              : GoogleFonts.poppins(
+                                                  // height: 1.2,
+                                                  color: Color(0xff1176B87),
+                                                  fontWeight: FontWeight.w800,
+                                                  fontSize: 18)),
+                                    ],
+                                  ));
+                            })),
+                    if (chatBloc.generating)
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Loading...", style: GoogleFonts.poppins(
+                                            color:
+                                                 Colors.blue[900],
+                                                 
+                                            fontWeight: FontWeight.w900,
+                                            fontSize: 20))
+                        ],
+                      ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 30, horizontal: 16),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: TextField(
+                            controller: textEditingController,
+                            style:GoogleFonts.poppins(
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue[600]),
+                            cursorColor: Theme.of(context).primaryColor,
+                            decoration: InputDecoration(
+                              
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                fillColor: Colors.white,
+                                hintText: "Your query goes here...",
+                                hintStyle:
+                                    GoogleFonts.poppins(
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue[600]),
+                                filled: true,
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(100),
+                                    borderSide: BorderSide(
+                                        color:
+                                            Colors.blue))),
+                          )),
+                          const SizedBox(width: 12),
+                          InkWell(
+                            onTap: () {
+                              if (textEditingController.text.isNotEmpty) {
+                                String text = textEditingController.text;
+                                textEditingController.clear();
+                                chatBloc.add(ChatGenerateNewTextMessageEvent(
+                                    inputMessage: text));
+                              }
+                            },
+                            child: CircleAvatar(
+                              foregroundColor: Colors.white,
+                              radius: 32,
+                              backgroundColor: Colors.white,
+                              child: CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Colors.blue[900],
+                                child: Center(
+                                  child: Icon(Icons.send, color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              );
+
+            default:
+              return SizedBox();
+          }
+        },
+      ),
+    );
   }
 }
